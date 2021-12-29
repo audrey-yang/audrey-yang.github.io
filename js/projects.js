271edea14b527213851c4960c0f4839d8a109622
audrey-yang <ayang24.robin@gmail.com>
2021-12-28 23:21:37 -0500

    End of 2021 update

--- js/projects.js
+++ js/projects.js
@@ -3,50 +3,73 @@ const src = document.getElementById("project-template").innerHTML;
 const template = Handlebars.compile(src);
 const compiledHtml = template({
     projects: [
-        {
-            date: "August 2020",
+        {   date: "Ongoing",
+            title: "PennApps Application Portal",
+            context: "PennApps",
+            description: "Customized system for both applicants and graders that encompassed the entire application, grading, and check-in process.",
+            tech: ["django"],
+            website: "https://apply.pennapps.com/"
+        },
+        {   date: "Fall 2021",
+            title: "Thrombotic Event Phenotyping from Clinical Notes",
+            context: "Independent Study",
+            description: "Pipeline reading clinical notes to look for descriptions of thrombotic events.",
+            tech: ["spacy", "scikit-learn"],
+        },
+        {   date: "Fall 2021",
+            title: "Markdown Parser",
+            context: "Functional Programming Final Project",
+            description: "Applicative parser translating Markdown to HTML.",
+            tech: ["haskell"],
+            src: "https://github.com/jasminecao/markdown-parser"
+        },
+        {   date: "Spring 2021",
+            title: "Can Twitter predict US county-level excessive alcohol consumption?",
+            context: "Computational Linguistics Final Project",
+            description: "ML models using language derived features from Twitter data to predict excessive alcohol consumption.",
+            tech: ["pandas", "scikit-learn", "pytorch"],
+            src: "https://github.com/logan-life/twitter_alcohol_use_prediction"
+        },
+        {   date: "Spring 2021",
+            title: "Reddit Posts and Stock Prices",
+            context: "Big Data Analytics Final Project",
+            description: "ML models finding the correlation between Reddit posts and stock market volatility.",
+            tech: ["pandas", "scikit-learn", "pytorch"],
+        },
+        {   date: "August 2020",
             title: "Seat Booker",
             context: "WCEC",
-            img: "./pics/project/clip-seatbooker.JPG",
             description: "Seat-reservation system created to help my church's COVID-19 reopening effort.",
-            tech: ["Django"],
+            tech: ["django"],
             website: "http://seatbooker.herokuapp.com/"
         },
-        {
-            date: "July 2020",
+        /*{   date: "July 2020",
             title: "Deep Sea Race",
             context: "HackItShipIt Hackathon",
-            img: "./pics/project/clip-deep-sea-race.JPG",
             description: "App introducing some ocean creatures and simulating of how fast the animals would move in a race.",
             tech: ["Java"],
             src: "https://github.com/gujiguj/Deep-Sea-Race"
         },
-        {
-            date: "June 2020",
+        {   date: "June 2020",
             title: "Productivity Sticky Space",
             context: "Same Home Different Hacks Hackathon",
-            img: "./pics/project/clip-board.JPG",
             description: "Simple bulletin-board app with moveable sticky notes.",
             tech: ["HTML", "CSS", "Javascript"],
             website: "https://gujiguj.github.io/productivity-sticky-space/",
             src: "https://github.com/gujiguj/productivity-sticky-space"
-        },
-        {
-            date: "June 2020",
+        },*/
+        {   date: "June 2020",
             title: "Mini-Instagram",
             context: "PennApps Development Team Challenge",
-            img: "./pics/project/clip-mini-instagram.JPG",
-            description: "Photo-sharing web application based off of Instagram. Users can upload images with captions and like and comment on their own and other users' photos.",
-            tech: ["Django"],
+            description: "Photo-sharing web application based off of Instagram.",
+            tech: ["django"],
             src: "https://github.com/audrey-yang/pennapps-dev-challenge"
         },
-        {
-            date: "December 2019",
+        {   date: "December 2019",
             title: "Gravity",
             context: "Programming Languages & Techniques Project",
-            img: "./pics/project/clip-gravity.JPG",
             description: "Game based off of Quizlet's Gravity – players upload vocabulary and input the definition for every item before time runs out.",
-            tech: ["Java"],
+            tech: ["java"],
             src: "https://github.com/audrey-yang/cis120-final-project"
         }
     ]

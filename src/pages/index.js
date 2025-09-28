cccb9a7a547c8af78f425258336f631c79942830
Audrey Yang <ayang.penn@gmail.com>
2025-09-28 09:23:16 -0400

    Update main page links

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,14 +15,18 @@ const IndexPage = () => {
 							i'm audrey. welcome to my corner of the internet.
 						</p>
 						<div className="flex flex-row content-between">
-							<p><Link className="navbutton" to="/about">résumé</Link></p>
-							<p><a className="navbutton" href="https://www.github.com/audrey-yang" 
+							<p><a className="link italic" href="https://www.github.com/audrey-yang" 
 								target="_blank" rel="noreferrer" title="GitHub">github</a></p>
-							<p><a className="navbutton" href="https://www.linkedin.com/in/audreyryang/" 
+							<p><a className="link italic" href="https://www.linkedin.com/in/audreyryang/" 
 								target="_blank" rel="noreferrer" title="Linkedin">linkedin</a></p>
-							<p><a className="navbutton" href="https://bsky.app/profile/aryang.bsky.social" 
+							<p><a className="link italic" href="https://blog.aryang.dev/" 
+								target="_blank" rel="noreferrer" title="Blog">blog</a></p>
+							<p><a className="link italic" href="https://bsky.app/profile/aryang.bsky.social" 
 								target="_blank" rel="noreferrer" title="Bluesky">bsky</a></p>
+							<p><a className="link italic" href="https://media.ebird.org/catalog?sort=obs_date_desc&userId=USER7850062&view=grid" 
+								target="_blank" rel="noreferrer" title="Macaulay Library">photography</a></p>
 						</div>
+						<p><a className="link" href="mailto:me@aryang.dev" title="Email me">say hi!</a></p>
 					</div>
 				</main>
 			</div>
